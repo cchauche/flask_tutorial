4c683a536300441574305db4575a8e09543e1453
Colin Chauche <colin.chauche@gmail.com>
2021-05-29 14:05:28 -0600

    Add auth blueprint

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -15,10 +15,10 @@ def create_app(test_config=None):
         # Load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        #Load the test config if passed in
+        # Load the test config if passed in
         app.config.from_mapping(test_config)
 
-    #ensure the instance folder exists
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
@@ -28,5 +28,11 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
