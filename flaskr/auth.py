8f1c2ab89700005639c3e69d9d45dd7c3a786a8d
Colin Chauche <colin.chauche@gmail.com>
2021-06-02 19:58:27 -0400

    Fix bug of passing too many args and user not defined

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -22,7 +22,7 @@ def register():
         elif not password:
             error = "Password is required."
         elif db.execute(
-            "SELECT id FROM user WHERE username = ?", (username)
+            "SELECT id FROM user WHERE username = ?", (username,)
         ).fetchone() is not None:
             error = f"User {username} is already registered."
 
@@ -44,8 +44,9 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
         db = get_db()
-        error = Noneuser = db.execute(
-            "SELECT * FROM user WHERE username = ?", (username)
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
         ).fetchone()
 
         if user is None:
@@ -70,7 +71,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get+db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id)
+            'SELECT * FROM user WHERE id = ?', (user_id,)
         ).fetchone()
 
 @bp.route('/logout')
