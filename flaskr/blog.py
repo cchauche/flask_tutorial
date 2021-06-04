d1ce84e5fa8a6f83a88b8625cc7caeb23aafc1df
Colin Chauche <colin.chauche@gmail.com>
2021-06-03 21:07:53 -0400

    Fix bug in blog.create

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -20,6 +20,7 @@ def index():
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
 
+
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
@@ -27,22 +28,25 @@ def create():
         title = request.form['title']
         body = request.form['body']
         error = None
+
         if not title:
             error = 'Title is required.'
 
-            if error is not None:
-                flash(error)
-            else:
-                db = get_db()
-                db.execute(
-                    'INSERT INTO post (title, body, author_id)'
-                    ' VALUES (?, ?, ?)',
-                    (title, body, g.user['id'])
-                )
-                db.commit()
-                return redirect(url_for('blog.index'))
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO post (title, body, author_id)'
+                ' VALUES (?, ?, ?)',
+                (title, body, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
 
-            return render_template('blog/create.html')
+    return render_template('blog/create.html')
+
+
 def get_post(id, check_author=True):
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
@@ -58,6 +62,7 @@ def get_post(id, check_author=True):
         abort(403)
 
     return post
+
 
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
@@ -85,7 +90,8 @@ def update(id):
 
     return render_template('blog/update.html', post=post)
 
-@bp.route('/<int:id>/delete', methods=('POST'))
+
+@bp.route('/<int:id>/delete', methods=('POST',))
 @login_required
 def delete(id):
     get_post(id)
